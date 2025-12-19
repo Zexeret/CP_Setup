b6423079fb58f73581aad04308d4971eb2bab245
Shyam Rathod <shyam.rathod2601@gmail.com>
2025-12-19 14:22:58 +0530

    Update template to include input parsing code

--- problem.cpp
+++ problem.cpp
@@ -1,30 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 typedef long long int ll;
 typedef long double ld;
- 
+
 const ll MOD = 1e9 + 7;
- 
+
+auto getInput()
+{
+    vector<ll> input;
+    string line;
+
+    while (getline(cin, line))
+    {
+
+        stringstream ss(line);
+        string num;
+        while (getline(ss, num, ','))
+        {
+            input.push_back(stoll(num));
+        }
+    }
+
+    return input;
+}
+
+ll removeDuplicates(vector<ll> &nums)
+{
+    int n = nums.size();
+
+    int preEle = nums[0];
+    for (auto it = nums.begin(); it != nums.end();)
+    {
+    
+        if (it == nums.begin())
+            preEle = *it;
+        else if (preEle == *it)
+        {
+            it = nums.erase(it);
+            continue;
+        }
+        else
+        {
+            preEle = *it;
+        }
+
+        ++it; 
+    }
+
+    return nums.size();
+}
+
 int solve()
 {
-    ll n ; cin >> n  ;
-    vector<ll> arr(n) ;
-    map<ll,ll> count ;
-    ll ans = 0 ;
-    for(int i = 0 ; i<n; i++) {
-        cin >> arr[i] ;
- 
-        count[arr[i]]++ ;
-        if (count[arr[i]] == 1) ans++ ;
+    auto inputArr = getInput();
+    ll answer = removeDuplicates(inputArr);
+
+    for (auto i : inputArr)
+    {
+        cout << i << " ";
     }
- 
-    cout << ans  ;
- 
- 
+    cout << endl;
+
+    cout << answer;
+
     return 0;
 }
- 
+
 int main()
 {
     ios_base::sync_with_stdio(0);
@@ -33,16 +75,16 @@ int main()
     cout << fixed << setprecision(10);
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
- 
-    //#########***************************FROM BELOW*******************************####################
- 
+
+    // #########***************************FROM BELOW*******************************####################
+
     ll t = 1;
-    //cin >> t;
- 
+    // cin >> t;
+
     while (t--)
     {
         solve();
     }
- 
+
     return 0;
 }
